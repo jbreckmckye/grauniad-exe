ec2f4a6589216f657ba6eb769766b1c90580a304
jbreckmckye <jimmy@breck-mckye.com>
2020-03-14 19:09:04 +0000

    Fetch commands

--- app/Fetch.hs
+++ app/Fetch.hs
@@ -2,14 +2,22 @@ module Fetch (
   fetchHtml
 ) where
 
-
 import Network.HTTP.Simple
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Char8 as B
 import Data.Text
+import Data.Text.Encoding
 
-fetchHtml :: IO L8.ByteString
-fetchHtml = do
-  req <- parseRequest "https://www.theguardian.com/uk"
-  response <- httpLBS req
-  return $ getResponseBody response
+homepage = pack "https://www.theguardian.com/uk"
+
+
+fetchHtml :: IO Text
+fetchHtml = html homepage
+
+
+html :: Text -> IO Text
+html url = do
+  req <- parseRequest (unpack url)
+  res <- httpBS req
+  let body = getResponseBody res
+  return $ decodeUtf8 body
