7f72644be94ee24e57f4ba51239000d11e529cf3
jbreckmckye <jimmy@breck-mckye.com>
2020-04-05 22:51:08 +0100

    Fix fetch

--- app/Fetch.hs
+++ app/Fetch.hs
@@ -8,16 +8,10 @@ import qualified Data.ByteString.Char8 as B
 import Data.Text
 import Data.Text.Encoding
 
-homepage = pack "https://www.theguardian.com/uk"
-
 
 fetchHtml :: IO Text
-fetchHtml = html homepage
-
-
-html :: Text -> IO Text
-html url = do
-  req <- parseRequest (unpack url)
+fetchHtml = do
+  req <- parseRequest "https://www.theguardian.com/uk"
   res <- httpBS req
   let body = getResponseBody res
   return $ decodeUtf8 body
